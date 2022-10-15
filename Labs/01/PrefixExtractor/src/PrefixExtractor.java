8499a4cf784d7005352eb23eee3f8052de9eddf0
Stefan Velev <sdvelev@gmail.com>
2022-10-15 11:35:35 +0300

    Add solution to Problem

---
+++ Labs/01/PrefixExtractor/src/PrefixExtractor.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class PrefixExtractor {
+    public static String getLongestCommonPrefix(String[] words) {
+        if(words == null){
+            return "";
+        }
+
+        Arrays.sort(words);
+
+        String first = words[0];
+        String last = words[words.length-1];
+
+        StringBuilder result = new StringBuilder("");
+
+        for(int i = 0; i < first.length(); i++){
+            if(first.charAt(i) == last.charAt(i)) {
+                result.append(first.charAt(i));
+                continue;
+            }
+            break;
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getLongestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+        System.out.println(getLongestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+        System.out.println(getLongestCommonPrefix(new String[]{"cat"}));
+    }
+}
