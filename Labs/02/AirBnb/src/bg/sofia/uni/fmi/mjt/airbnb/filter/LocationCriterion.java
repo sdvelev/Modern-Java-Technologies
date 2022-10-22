0fec4df3647499b8281f594c8cce6bdd87171a2d
Stefan Velev <sdvelev@gmail.com>
2022-10-22 15:13:43 +0300

    Upload ready filter packet classes

--- Labs/02/AirBnb/src/bg/sofia/uni/fmi/mjt/airbnb/filter/LocationCriterion.java
+++ Labs/02/AirBnb/src/bg/sofia/uni/fmi/mjt/airbnb/filter/LocationCriterion.java
@@ -1,4 +1,32 @@
 package bg.sofia.uni.fmi.mjt.airbnb.filter;
 
-public class LocationCriterion {
+import bg.sofia.uni.fmi.mjt.airbnb.accommodation.Bookable;
+import bg.sofia.uni.fmi.mjt.airbnb.accommodation.location.Location;
+
+public class LocationCriterion implements Criterion{
+
+    private final Location currentLocation;
+    private final double maxDistance;
+
+    public LocationCriterion(Location currentLocation, double maxDistance){
+        this.currentLocation = currentLocation;
+        this.maxDistance = maxDistance;
+    }
+
+    /**
+     * @param bookable
+     * @return true, if the bookable matches the criterion. If bookable is null, returns false.
+     */
+    @Override
+    public boolean check(Bookable bookable) {
+        if(bookable == null){
+            return false;
+        }
+
+        if(Math.sqrt(Math.pow((Math.abs(this.currentLocation.getX()-bookable.getLocation().getX())),2) +
+                Math.pow((Math.abs(this.currentLocation.getY()-bookable.getLocation().getY())),2)) <= this.maxDistance){
+            return true;
+        }
+        return false;
+    }
 }
--- Labs/02/AirBnb/src/bg/sofia/uni/fmi/mjt/airbnb/filter/PriceCriterion.java
+++ Labs/02/AirBnb/src/bg/sofia/uni/fmi/mjt/airbnb/filter/PriceCriterion.java
@@ -1,4 +1,30 @@
 package bg.sofia.uni.fmi.mjt.airbnb.filter;
 
-public class PriceCriterion {
+import bg.sofia.uni.fmi.mjt.airbnb.accommodation.Bookable;
+
+public class PriceCriterion implements Criterion{
+
+    private final double minPrice;
+    private final double maxPrice;
+
+    public PriceCriterion(double minPrice, double maxPrice){
+        this.minPrice=minPrice;
+        this.maxPrice=maxPrice;
+    }
+
+    /**
+     * @param bookable
+     * @return true, if the bookable matches the criterion. If bookable is null, returns false.
+     */
+    @Override
+    public boolean check(Bookable bookable) {
+        if (bookable == null){
+            return false;
+        }
+
+        if (bookable.getPricePerNight() >= minPrice && bookable.getPricePerNight() <= maxPrice){
+            return true;
+        }
+        return false;
+    }
 }
