1b0f1544533946658f00d84f9e0b1c5606b20b7b
Stefan Velev <sdvelev@gmail.com>
2022-12-17 21:59:06 +0200

    Add initial solution to Lab 09

---
+++ Labs/09/MJTGradingSimulator/src/bg/sofia/uni/fmi/mjt/grading/simulator/Assistant.java
@@ -0,0 +1,153 @@
+package bg.sofia.uni.fmi.mjt.grading.simulator;
+
+import bg.sofia.uni.fmi.mjt.grading.simulator.assignment.Assignment;
+import bg.sofia.uni.fmi.mjt.grading.simulator.grader.AdminGradingAPI;
+import bg.sofia.uni.fmi.mjt.grading.simulator.grader.CodePostGrader;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Assistant extends Thread {
+
+    private final String name;
+    private CodePostGrader grader;
+
+    private AtomicInteger numberOfGradedAssignments;
+
+    public Assistant(String name, CodePostGrader grader) {
+
+        this.name = name;
+        this.grader = grader;
+        this.numberOfGradedAssignments = new AtomicInteger();
+        this.numberOfGradedAssignments.set(0);
+    }
+
+    @Override
+    public void run() {
+
+        Assignment toCheckAssignment;
+
+        while ((toCheckAssignment = this.grader.getAssignment()) != null) {
+
+            try {
+
+                Thread.sleep(toCheckAssignment.type().getGradingTime());
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+
+            this.numberOfGradedAssignments.incrementAndGet();
+
+        }
+
+        System.out.println(this.name + " " + this.numberOfGradedAssignments.get());
+
+    }
+        /*while (true) {
+
+
+            Assignment toCheckAssignment = this.grader.getAssignment();
+
+            if (toCheckAssignment == null) {
+
+                if (this.grader.getIsFinalized()) {
+                    break;
+                } else {
+
+                    try {
+
+                        this.wait();
+                    } catch (InterruptedException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+
+                try {
+
+                    Thread.sleep(toCheckAssignment.type().getGradingTime());
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+
+                this.numberOfGradedAssignments.incrementAndGet();
+            }
+        }
+
+        System.out.println(this.name + " " + this.numberOfGradedAssignments.get());
+
+    }
+*/
+
+
+/*
+
+        while (true) {
+
+
+            if (this.grader.getIsFinalized()) {
+
+                while (true) {
+
+                    Assignment toCheckAssignment = this.grader.getAssignment();
+
+                    if (toCheckAssignment != null) {
+
+                        try {
+
+                            Thread.sleep(toCheckAssignment.type().getGradingTime());
+                        } catch (InterruptedException e) {
+
+                            e.printStackTrace();
+                        }
+
+                        this.numberOfGradedAssignments.incrementAndGet();
+
+                        //System.out.println("Work checked! There are " + numberOfGradedAssignments.get() + " graded assignments!");
+                    } else {
+                        break;
+                    }
+
+                }
+            }
+
+            Assignment toCheckAssignment = this.grader.getAssignment();
+
+            if (toCheckAssignment == null) {
+                try {
+
+                    this.wait();
+                }
+                catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+            }
+            else {
+
+                try {
+
+                    Thread.sleep(toCheckAssignment.type().getGradingTime());
+                }
+                catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+
+                this.numberOfGradedAssignments.incrementAndGet();
+
+                //System.out.println("Work checked! There are " + numberOfGradedAssignments.get() + " graded assignments!");
+            }
+        }
+
+       // System.out.println(this.name + " " + this.numberOfGradedAssignments);
+    }*/
+
+    public int getNumberOfGradedAssignments() {
+
+        return this.numberOfGradedAssignments.get();
+    }
+
+}
---
+++ Labs/09/MJTGradingSimulator/src/bg/sofia/uni/fmi/mjt/grading/simulator/Student.java
@@ -0,0 +1,79 @@
+package bg.sofia.uni.fmi.mjt.grading.simulator;
+
+import bg.sofia.uni.fmi.mjt.grading.simulator.assignment.Assignment;
+import bg.sofia.uni.fmi.mjt.grading.simulator.assignment.AssignmentType;
+import bg.sofia.uni.fmi.mjt.grading.simulator.grader.StudentGradingAPI;
+
+import java.util.Random;
+
+public class Student implements Runnable {
+
+    private static final int MAX_ASSIGNMENT_CREATION_TIME = 1001;
+    private static final int MAX_ASSIGNMENT_TYPE = 4;
+
+    private static final int MAX_STUDENT_FN = 7000;
+
+    private static final int LAB_OPTION = 0;
+    private static final int PLAYGROUND_OPTION = 1;
+    private static final int HOMEWORK_OPTION = 2;
+    private static final int PROJECT_OPTION = 3;
+    private static final Random RND = new Random();
+
+    //private static int ASSIGNMENT_COUNT = 0;
+    private final int fn;
+    private final String name;
+    private StudentGradingAPI studentGradingAPI;
+
+    public Student(int fn, String name, StudentGradingAPI studentGradingAPI) {
+
+        this.fn = fn;
+        this.name = name;
+        this.studentGradingAPI = studentGradingAPI;
+    }
+
+    @Override
+    public void run() {
+
+        int waitingTime = RND.nextInt(MAX_ASSIGNMENT_CREATION_TIME);
+
+        int assignmentType = RND.nextInt(MAX_ASSIGNMENT_TYPE);
+
+        AssignmentType toAddAssignmentType = switch (assignmentType) {
+
+            case LAB_OPTION -> AssignmentType.LAB;
+            case PLAYGROUND_OPTION -> AssignmentType.PLAYGROUND;
+            case HOMEWORK_OPTION -> AssignmentType.HOMEWORK;
+            case PROJECT_OPTION -> AssignmentType.PROJECT;
+            default -> null;
+        };
+
+        try {
+
+            Thread.sleep(waitingTime);
+        }
+        catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
+
+        Assignment toAddAssignment = new Assignment(this.fn, this.name, toAddAssignmentType);
+        this.studentGradingAPI.submitAssignment(toAddAssignment);
+       // System.out.println("Assignment submitted!");
+    }
+
+    public int getFn() {
+
+        return fn;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public StudentGradingAPI getGrader() {
+
+        return studentGradingAPI;
+    }
+
+}
